052e6ffd49a0b35ac59100c410691d9da7bf960b
fmartintbx <facundo.corvalan@mi.unc.edu.ar>
2024-08-17 01:40:49 -0300

    useEffect

--- src/TaskLists.jsx
+++ src/TaskLists.jsx
@@ -1,27 +1,25 @@
-
-import { tasks } from './tasks'
+import { tasks as data } from './tasks'
 import { useState, useEffect } from 'react'
 
-console.log(tasks)
 export default function TaskLists() {
-   
   const [tasks, setTasks] = useState([])
-  
-  useEffect(() => { 
-   setTasks(tasks)
+
+  useEffect(() => {
+    setTasks(data)
   }, [])
 
   if (tasks.length === 0) {
-       return <h1>No hay tareas aun</h1>
+    return <h1>No hay tareas aun</h1>
   }
-  return (
-    <div> 
-      { 
-        tasks.map(task =>  {  
-        <div>tarea</div>
 
-        })
-      }
+  return (
+    <div>
+      {tasks.map(task => (
+        <div key={task.id}>
+          <h1>{task.title}</h1>
+          <p>{task.description}</p>
+        </div>
+      ))}
     </div>
   )
 }
